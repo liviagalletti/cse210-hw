435e6b79f8fa6ae6c922d222b061aa98cf564824
liviagalletti <liligalletti@gmail.com>
2023-09-23 20:53:38 -0300

    guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Data.Common;
+using System.Reflection.Metadata;
+
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        int guess;
+
+        do
+        {
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+        } while (guess != number);
+
+        Console.WriteLine("You guessed it!");
     }
 }
