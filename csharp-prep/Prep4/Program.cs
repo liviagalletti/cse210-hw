3a22d2c532b5a789f42d993fcbd989993589b97b
liviagalletti <liligalletti@gmail.com>
2023-09-24 16:50:21 -0300

    c# list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> number = new List<int>();
+        int newNumber;
+
+        do {
+            Console.WriteLine("Enter number:");
+            newNumber = int.Parse(Console.ReadLine());
+            number.Add(newNumber);
+
+        } while (newNumber != 0 );
+
+        foreach (int num in number){
+
+            Console.WriteLine(num + " ");
+        }
     }
 }
